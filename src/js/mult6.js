8b86f3247a14f23361631b992a11553c1f948901
Dan Schlosser <dan.r.schlosser@gmail.com>
2020-07-19 19:45:22 -0400

    clean up mult-6 logic

--- src/js/mult6.js
+++ src/js/mult6.js
@@ -1,1 +1,19 @@
-// todo: on resize make everything always a multiple of 6rem;
+document.addEventListener('DOMContentLoaded', function () {
+  var textElems = document.getElementsByClassName('stretch-to-mult');
+
+  function stretchAsNeeded() {
+    var i, naturalHeight, remainder;
+    var rem = parseFloat(window.getComputedStyle(document.body).fontSize);
+    for (i = 0; i < textElems.length; i++) {
+      textElems[i].style.height = 'auto';
+      naturalHeight = parseFloat(window.getComputedStyle(textElems[i]).height);
+      remainder = naturalHeight % (6 * rem);
+      if (remainder !== 0) {
+        textElems[i].style.height = (naturalHeight + (6 * rem) - remainder) + "px";
+      }
+    }
+  }
+
+  window.addEventListener('resize', stretchAsNeeded);
+  stretchAsNeeded();
+});
